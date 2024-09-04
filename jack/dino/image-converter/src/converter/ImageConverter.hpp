ffcaef4b709c222aeea442458a22f2c60e0d24aa
happytomatoe <2893931+happytomatoe@users.noreply.github.com>
2024-09-04 02:49:34 +0200

    WIP

--- jack/dino/image-converter/src/converter/ImageConverter.hpp
+++ jack/dino/image-converter/src/converter/ImageConverter.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cmath>
+#include <iostream>
 #include <ostream>
+#include <ranges>
 #include <regex>
 #include <string>
 #include <re2/re2.h>
@@ -13,18 +15,54 @@ struct Image {
     vector<vector<uint16_t> > pixels;
 
     Image(int height, int width, const uint16_t *pixels) {
-        cols = round((float) width / 16);
+        auto c = round((float) width / 8 / 2);
+
+        cols = c;
         rows = height;
         //convert 1d vector to 2d array
+        auto jLen = width / 8;
         for (int i = 0; i < height; ++i) {
             vector<uint16_t> temp;
             temp.reserve(width);
-            for (int j = 1; j < width / 8; j += 2) {
+            for (int j = 1; j < jLen; j += 2) {
                 auto val = pixels[i * width / 8 + j - 1] << 8 | pixels[i * width / 8 + j];
                 temp.push_back(~val);
             }
+            if (jLen % 2 != 0) {
+                auto last_val = pixels[i * width / 8 + jLen - 1];
+                auto val = last_val << 8 | 0b11111111;
+                temp.push_back(~val);
+            }
             std::reverse(temp.begin(), temp.end());
-            this->pixels.push_back(temp);
+            //transform temp to string
+            auto str = temp |
+                       views::transform([](auto &&val) {
+                           std::bitset<16> b(val);
+                           return b.to_string();
+                       }) | views::join;
+
+            //res to string
+            string s2;
+            for (auto c: str) {
+                s2 += c;
+            }
+            auto len = s2.length();
+            cout << len;
+            reverse(s2.begin(), s2.end());
+            auto temp2 = s2 |
+                         views::chunk(16) |
+                         views::transform([](auto &&chunk) {
+                             string res;
+                             for (auto c: chunk) {
+                                 res += c;
+                             }
+                             std::bitset<16> b(res);
+                             uint16_t number = static_cast<uint16_t>(b.to_ullong() & 0xFFFF);
+                             return number;
+                         })
+                         //to vector
+                         | ranges::to<vector<uint16_t> >();
+            this->pixels.push_back(temp2);
         }
     }
 
